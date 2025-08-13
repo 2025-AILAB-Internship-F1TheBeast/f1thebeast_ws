2ad88331633b370cbd831f8ae812ced7781dfd90
kkwoned <o990703@gmail.com>
2025-08-13 17:51:20 +0900

    for checkout

--- learning/learning_localplanner.py
+++ learning/learning_localplanner.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
 
+class DataLoader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = pd.read_csv(self.file_path)
+        self.train_data, self.test_data = self.split_data()
 
-# Data Loading
-def load_data(file_path):
-    data = pd.read_csv(file_path)
-    return data
+    def split_data(self, train_size=0.8):
+        train_data = self.data.sample(frac=train_size, random_state=42)
+        test_data = self.data.drop(train_data.index)
+        return train_data, test_data
+    def layer
